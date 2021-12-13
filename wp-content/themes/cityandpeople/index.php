aa4bb91ca6361224bb991c5523a9ea2c5802a28c
Alex Ivanov <arhibober@gmail.com>
2021-12-13 11:46:31 +0200

    Pagination and content were added

--- wp-content/themes/cityandpeople/index.php
+++ wp-content/themes/cityandpeople/index.php
@@ -1,39 +1,37 @@
-<?php get_header();?>
+<?php get_header("v2");?>
 <!-- Page Content -->
 <div class="container">
-
     <h1 class="my-4">Welcome to Modern Business</h1>
-
     <!-- Marketing Icons Section -->
     <div class="row">
-        <div class="col-lg-4 mb-4">
-            <div class="card h-100">
-                <h4 class="card-header">Card Title</h4>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse
-                        necessitatibus neque.</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="btn btn-primary">Learn More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-            <div class="card h-100">
-                <h4 class="card-header">Card Title</h4>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam
-                        eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque
-                        exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="btn btn-primary">Learn More</a>
-                </div>
-            </div>
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <h1 class="my-4">Page Heading
+                <small>Secondary Text</small>
+            </h1>
+            <?php if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        get_template_part('partials/posts/content', 'excerpt');
+    }
+}
+?>
+
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item">
+                    <?php previous_posts_link("&larr; Older");?>
+                    <!-- <a class="page-link" href="#">&larr; Older</a> -->
+                </li>
+                <li class="page-item">
+                    <?php next_posts_link("Newer &rarr;");?>
+                    <!-- <a class="page-link" href="#">Newer &rarr;</a> -->
+                </li>
+            </ul>
         </div>
         <?php get_sidebar();?>
+        <!-- /.row -->
     </div>
-
 </div>
 <!-- /.container -->
 
