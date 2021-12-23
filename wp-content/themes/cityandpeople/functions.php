3f505e2bc07b12a3eabc3d59df047db7c06dfe59
Alex Ivanov <arhibober@gmail.com>
2021-12-23 22:37:14 +0200

    Files was sorted by dirs

--- wp-content/themes/cityandpeople/functions.php
+++ wp-content/themes/cityandpeople/functions.php
@@ -4,21 +4,21 @@
 define('CITYANDPEOPLE_DEV_MODE', true);
 
 // Includes
-include get_theme_file_path('includes/Enqueue.php');
-include get_theme_file_path('includes/Setup.php');
-include get_theme_file_path('includes/Widgets.php');
-include get_theme_file_path('includes/Cityandpeople_Nav_Walker.php');
-include get_theme_file_path('includes/Custom_Post_Types.php');
-include get_theme_file_path('includes/Custom_Fields_Type.php');
+include get_theme_file_path('includes/core/Enqueue.php');
+include get_theme_file_path('includes/core/Setup.php');
+include get_theme_file_path('includes/core/Widgets.php');
+include get_theme_file_path('includes/core/Cityandpeople_Nav_Walker.php');
+include get_theme_file_path('includes/core/Custom_Post_Types.php');
+include get_theme_file_path('includes/core/Custom_Fields_Type.php');
 //include get_theme_file_path('includes/acf-fields.php');
 //include get_theme_file_path('includes/filter_rating.php');
-include get_theme_file_path('includes/Acf_Filter_Rating.php');
-include get_theme_file_path('includes/Acf_Filter_Year.php');
+include get_theme_file_path('includes/filters/Acf_Filter_Rating.php');
+include get_theme_file_path('includes/filters/Acf_Filter_Year.php');
 //include get_theme_file_path('includes/filter_year.php');
 //include get_theme_file_path('includes/acf-slider.php');
-include get_theme_file_path('includes/My_Slider.php');
-include get_theme_file_path('includes/Cpt_Gutenberg_Support.php');
-include get_theme_file_path('includes/Gutenberg_Template_To_Single_Post.php');
+include get_theme_file_path('includes/core/My_Slider.php');
+include get_theme_file_path('includes/core/Cpt_Gutenberg_Support.php');
+include get_theme_file_path('includes/core/Gutenberg_Template_To_Single_Post.php');
 // Hooks
 add_action('wp_enqueue_scripts', [new Enqueue(), 'cityandpeople_enqueue']);
 add_action('after_setup_theme', [new Setup(), 'cityandpeople_setup_theme']);
